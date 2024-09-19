d9845a552e48907d67688375404f3f4168b8e264
Leekangmin <kkk5474096@naver.com>
2024-09-19 18:43:57 +0900

    [chore/#4] neodgm 폰트 추가

--- app/src/main/java/com/ddanddan/ddanddan/util/extension/FloatExt.kt
+++
@@ -1,9 +0,0 @@
-package com.ddanddan.ddanddan.util.extension
-
-import android.content.Context
-
-fun Float.dpToPx(context: Context): Int {
-    return context.resources.displayMetrics.density.let { density ->
-        (this * density).toInt()
-    }
-}
