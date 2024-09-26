af010c70cca8a85d25a7b87abdb06557637821d2
Leekangmin <kkk5474096@naver.com>
2024-09-26 18:10:26 +0900

    [feature/#14] HomeActivity -> MainActivity 변경

--- app/src/main/java/com/ddanddan/ddanddan/presentation/MainActivity.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.ddanddan.ddanddan.presentation
 
 import android.os.Bundle
-import com.ddanddan.ui.base.BindingActivity
-import com.ddanddan.ddanddan.R
-import com.ddanddan.ddanddan.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.ddanddan.ui.compose.DDanDDanColorPalette
+import com.ddanddan.ui.compose.theme.DDanDDanTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            DDanDDanTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = DDanDDanColorPalette.current.color_background) {
+                    MainScreen()
+                }
+            }
+        }
     }
 }
