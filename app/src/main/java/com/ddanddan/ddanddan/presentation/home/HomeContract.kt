4451ac95ea462af4db0aeecb144ea3ac840faeb4
Leekangmin <kkk5474096@naver.com>
2024-10-18 14:09:39 +0900

    [refactoring/#30] HomeState Immutable 변경

--- app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeContract.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeContract.kt
@@ -1,8 +1,10 @@
 package com.ddanddan.ddanddan.presentation.home
 
+import androidx.compose.runtime.Immutable
 import com.ddanddan.domain.entity.Pet
 import com.ddanddan.domain.entity.User
 
+@Immutable
 data class HomeState(
     val isLoading: Boolean = false,
     val user: User? = null,
