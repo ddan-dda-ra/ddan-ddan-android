e1ba83ce3017538220661375b4bc59a6ad55533b
blueme0 <ksytpdus0304@gmail.com>
2024-10-21 16:14:00 +0900

    [feature/#29] putUser api

--- core/data/src/main/java/com/ddanddan/data/service/UserService.kt
+++ core/data/src/main/java/com/ddanddan/data/service/UserService.kt
@@ -2,16 +2,23 @@ package com.ddanddan.data.service
 
 import com.ddanddan.model.request.RequestLogin
 import com.ddanddan.model.request.RequestMainPet
+import com.ddanddan.model.request.RequestUser
 import com.ddanddan.model.response.ResponseLogin
 import com.ddanddan.model.response.ResponseMainPet
 import com.ddanddan.model.response.ResponseUser
 import retrofit2.http.GET
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface UserService {
     @GET("/v1/user/me")
     suspend fun getUser(): ResponseUser
+
+    @PUT("/v1/user/me")
+    suspend fun putUser(
+        @Body requestUser: RequestUser
+    ): ResponseUser
 
     @GET("/v1/user/me/main-pet")
     suspend fun getMainPet(): ResponseMainPet
