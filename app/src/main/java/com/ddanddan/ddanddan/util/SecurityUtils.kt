66269fd7cc9c884b8f445e8a1b2712599341ce03
nami <dnska6657@naver.com>
2024-11-12 15:05:28 +0900

    [CHORE/#45] 사용하지 않는 import 제거

--- app/src/main/java/com/ddanddan/ddanddan/util/SecurityUtils.kt
+++ app/src/main/java/com/ddanddan/ddanddan/util/SecurityUtils.kt
@@ -1,16 +1,9 @@
 package com.ddanddan.ddanddan.util
 
-import android.content.Context
 import android.util.Base64
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
 import com.ddanddan.ddanddan.BuildConfig
 import timber.log.Timber
-import java.security.KeyFactory
-import java.security.PublicKey
-import java.security.spec.X509EncodedKeySpec
 import javax.crypto.Cipher
-import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 object SecurityUtils {
