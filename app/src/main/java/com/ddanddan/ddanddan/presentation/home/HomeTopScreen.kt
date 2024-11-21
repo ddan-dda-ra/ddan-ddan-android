b54d683ec317f1d7ddfad43b6ccac833f9441844
Leekangmin <kkk5474096@naver.com>
2024-11-21 18:40:11 +0900

    [feature/#55] 홈 상단 뷰 리컴포지션 최적화

--- app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeTopScreen.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeTopScreen.kt
@@ -7,14 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ddanddan.ddanddan.R
-import com.ddanddan.ui.compose.component.DDanTwoButtonDialog
 
 @Composable
 fun HomeTopScreen(onStorageClick: () -> Unit = {}, onSettingClick: () -> Unit = {}) {
@@ -25,16 +22,12 @@ fun HomeTopScreen(onStorageClick: () -> Unit = {}, onSettingClick: () -> Unit = 
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
-            modifier = Modifier.clickable {
-                onStorageClick()
-            },
+            modifier = Modifier.clickable(onClick = onStorageClick),
             painter = painterResource(id = R.drawable.ic_storage),
             contentDescription = "보관함"
         )
         Image(
-            modifier = Modifier.clickable {
-                onSettingClick()
-            },
+            modifier = Modifier.clickable(onClick = onSettingClick),
             painter = painterResource(id = R.drawable.ic_setting),
             contentDescription = "설정"
         )
