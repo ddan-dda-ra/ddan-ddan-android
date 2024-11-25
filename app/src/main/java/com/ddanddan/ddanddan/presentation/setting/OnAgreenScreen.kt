e7d61e2032fd751cc8fb0d21def8f86aed462c08
Leekangmin <kkk5474096@naver.com>
2024-11-25 19:05:46 +0900

    [feature/#59] 약관 배경색 버그 수정

--- app/src/main/java/com/ddanddan/ddanddan/presentation/setting/OnAgreenScreen.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/setting/OnAgreenScreen.kt
@@ -3,10 +3,9 @@ package com.ddanddan.ddanddan.presentation.setting
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,8 +33,7 @@ fun onAgreeScreen(
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
+                .fillMaxSize()
                 .background(color = DDanDDanColorPalette.current.color_background),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
