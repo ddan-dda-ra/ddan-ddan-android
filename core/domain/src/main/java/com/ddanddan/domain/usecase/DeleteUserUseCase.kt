053ec52269ad258a774ea61fd21d63138e382492
Leekangmin <kkk5474096@naver.com>
2024-11-28 10:21:29 +0900

    [feature/#59] DeleteUserUseCase 로직 변경

--- core/domain/src/main/java/com/ddanddan/domain/usecase/DeleteUserUseCase.kt
+++ core/domain/src/main/java/com/ddanddan/domain/usecase/DeleteUserUseCase.kt
@@ -1,12 +1,22 @@
 package com.ddanddan.domain.usecase
 
+import com.ddanddan.domain.repository.AuthRepository
 import com.ddanddan.domain.repository.UserRepository
 import javax.inject.Inject
 
 class DeleteUserUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke() = runCatching {
         userRepository.deleteUser()
-    }
+    }.fold(
+        onSuccess = {
+            if (it) {
+                authRepository.disableAutoLogin()
+            }
+            it
+        }, onFailure = {
+            false
+        })
 }
