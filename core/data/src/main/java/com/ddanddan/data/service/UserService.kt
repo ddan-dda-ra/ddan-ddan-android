9e2b449dac05d6cbc8f90e432a6fee025f8058d0
Leekangmin <kkk5474096@naver.com>
2024-12-09 09:39:47 +0900

    [feature/#71] 회원탈퇴 http 변경

--- core/data/src/main/java/com/ddanddan/data/service/UserService.kt
+++ core/data/src/main/java/com/ddanddan/data/service/UserService.kt
@@ -11,6 +11,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
@@ -23,7 +24,7 @@ interface UserService {
         @Body requestUser: RequestUser
     ): ResponseUser
 
-    @DELETE("/v1/users/me")
+    @HTTP(method = "DELETE", path = "/v1/users/me", hasBody = true)
     suspend fun deleteUser(
         @Body request: RequestSignOut
     ): Response<Unit>
