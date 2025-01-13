a53b7ca144035119012652b6adb975a5376be7c5
blueme0 <ksytpdus0304@gmail.com>
2025-01-13 18:39:11 +0900

    [feat/#102] version 2 -> 3

--- build-logic/convention/src/main/java/com/ddanddan/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/java/com/ddanddan/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 2
+    const val versionCode = 3
     const val versionName = "1.0"
     const val jvmVersion = "17"
 }
