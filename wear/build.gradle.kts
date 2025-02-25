696b73510c741e601831b37ad909c852dbf39dd9
Leekangmin <kkk5474096@naver.com>
2025-02-25 16:07:21 +0900

    [feature/#107] wear flavor prod, dev 구현

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ddanddan.ddanddan"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
+        versionCode = 8
         versionName = "1.0"
     }
 
@@ -30,20 +30,7 @@ android {
     }
 
     buildTypes {
-        debug {
-            buildConfigField(
-                "String",
-                "BASE_URL",
-                gradleLocalProperties(rootDir).getProperty("dev.base.url"),
-            )
-        }
         release {
-            buildConfigField(
-                "String",
-                "BASE_URL",
-                gradleLocalProperties(rootDir).getProperty("base.url"),
-            )
-            
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -51,6 +38,22 @@ android {
             )
         }
     }
+
+    flavorDimensions.add("environment")
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+
+            buildConfigField("String", "BASE_URL", gradleLocalProperties(rootDir).getProperty("dev.base.url"))
+        }
+        create("prod") {
+            dimension = "environment"
+
+            buildConfigField("String", "BASE_URL", gradleLocalProperties(rootDir).getProperty("base.url"))
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
