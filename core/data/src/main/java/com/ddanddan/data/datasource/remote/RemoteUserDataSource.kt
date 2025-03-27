6ef93f9b0e6bac5b4514603bd7e6e673dcde2149
Leekangmin <kkk5474096@naver.com>
2025-03-27 19:19:31 +0900

    [feature/#109] DataSource deviceToken 추가

--- core/data/src/main/java/com/ddanddan/data/datasource/remote/RemoteUserDataSource.kt
+++ core/data/src/main/java/com/ddanddan/data/datasource/remote/RemoteUserDataSource.kt
@@ -21,5 +21,5 @@ class RemoteUserDataSource @Inject constructor(
     }
     suspend fun getMainPet(): ResponseMainPet = userService.getMainPet()
     suspend fun postMainPet(petId: String) = userService.postMainPet(RequestMainPet(petId))
-    suspend fun postLogin(token: String) = userService.postLogin(RequestLogin(token, "KAKAO"))
+    suspend fun postLogin(token: String, deviceToken: String?) = userService.postLogin(RequestLogin(token, "KAKAO", deviceToken))
 }
