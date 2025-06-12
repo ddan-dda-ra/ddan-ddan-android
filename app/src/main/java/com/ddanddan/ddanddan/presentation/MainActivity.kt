e5139ef0be6ed1e73fd51cff005b8ca7290dfc7a
Leekangmin <kkk5474096@naver.com>
2025-06-12 12:46:27 +0900

    [feature/#126] MainScreen 변경

--- app/src/main/java/com/ddanddan/ddanddan/presentation/MainActivity.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ddanddan.ddanddan.presentation
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.ddanddan.ddanddan.service.PhoneDataLayerService
 import com.ddanddan.ui.compose.DDanDDanColorPalette
 import com.ddanddan.ui.compose.theme.DDanDDanTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,8 +25,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DDanDDanTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = DDanDDanColorPalette.current.color_background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = DDanDDanColorPalette.current.color_background
+                ) {
+                    
                     MainScreen()
                 }
             }
