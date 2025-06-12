68bdb8edbf36a82023d86cdab6df2c62c106f2cb
Leekangmin <kkk5474096@naver.com>
2025-06-12 12:45:25 +0900

    [feature/#126] 스플래시에서 권한 체크 추가

--- app/src/main/java/com/ddanddan/ddanddan/presentation/splash/SplashContract.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/splash/SplashContract.kt
@@ -4,5 +4,6 @@ sealed class SplashSideEffect {
     object NetworkError: SplashSideEffect()
     object NavigateOnboarding: SplashSideEffect()
     object NavigateSignIn: SplashSideEffect()
+    object NavigateGrantNotPermission: SplashSideEffect()
     object NavigateHome: SplashSideEffect()
 }
--- app/src/main/java/com/ddanddan/ddanddan/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/splash/SplashScreen.kt
@@ -1,5 +1,9 @@
 package com.ddanddan.ddanddan.presentation.splash
 
+import android.Manifest
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +17,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ddanddan.base.R
 import com.ddanddan.ddanddan.util.NetworkManager
@@ -35,6 +43,7 @@ fun SplashRoute(
     splashViewModel: SplashViewModel = hiltViewModel(),
     onNavigateHome: () -> Unit,
     onNavigateSignIn: () -> Unit,
+    onNavigateGrantNotPermission: () -> Unit,
     onNavigateOnboarding: () -> Unit
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
@@ -44,6 +53,7 @@ fun SplashRoute(
             is SplashSideEffect.NavigateSignIn -> onNavigateSignIn()
             is SplashSideEffect.NavigateHome -> onNavigateHome()
             is SplashSideEffect.NavigateOnboarding -> onNavigateOnboarding()
+            is SplashSideEffect.NavigateGrantNotPermission -> onNavigateGrantNotPermission()
             is SplashSideEffect.NetworkError -> snackBarHostState.showSnackbar("인터넷 연결이 필요합니다.")
         }
     }
@@ -51,7 +61,8 @@ fun SplashRoute(
     SplashScreen(
         snackBarHostState = snackBarHostState,
         onCheckNavigate = splashViewModel::isFirstAfterInstall,
-        onNetworkDisconnected = splashViewModel::disconnectedNetwork
+        onNetworkDisconnected = splashViewModel::disconnectedNetwork,
+        onGrantNotPermission = splashViewModel::grantNotPermission
     )
 }
 
@@ -60,9 +71,22 @@ fun SplashRoute(
 fun SplashScreen(
     snackBarHostState: SnackbarHostState = remember { SnackbarHostState() },
     onCheckNavigate: () -> Unit = { },
-    onNetworkDisconnected: () -> Unit = { }
+    onNetworkDisconnected: () -> Unit = { },
+    onGrantNotPermission: () -> Unit = { }
 ) {
     val context = LocalContext.current
+
+    var hasPermission by remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            onCheckNavigate()
+        } else {
+            onGrantNotPermission()
+        }
+    }
 
     LaunchedEffect(Unit) {
         if (!NetworkManager.checkNetworkState(context)) {
@@ -70,7 +94,16 @@ fun SplashScreen(
             return@LaunchedEffect
         }
         delay(3000)
-        onCheckNavigate()
+
+        hasPermission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.BODY_SENSORS
+        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        if (hasPermission) {
+            onCheckNavigate()
+        } else {
+            permissionLauncher.launch(Manifest.permission.BODY_SENSORS)
+        }
     }
 
     Scaffold(
