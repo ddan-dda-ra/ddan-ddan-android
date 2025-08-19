c49b21772c73804b2b423bb0a257b9a6116b7696
Leekangmin <kkk5474096@naver.com>
2025-08-19 18:13:04 +0900

    [feature/#141] SDK 35 대응

--- build-logic/convention/src/main/java/com/ddanddan/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/java/com/ddanddan/convention/src/main/kotlin/Constants.kt
@@ -1,9 +1,9 @@
 object Constants {
     const val packageName = "com.ddanddan.ddanddan"
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 30
-    const val targetSdk = 34
-    const val versionCode = 16
+    const val targetSdk = 35
+    const val versionCode = 20
     const val versionName = "1.0"
     const val jvmVersion = "17"
 }
