d9aa9323cc412f7dba519ae34f7847c63bbc51b7
Leekangmin <kkk5474096@naver.com>
2025-09-09 10:08:02 +0900

    [feature/#146] android core splash 적용

--- wear/src/main/java/com/ddanddan/ddanddan/presentation/kangmin/NewMainActivity.kt
+++ wear/src/main/java/com/ddanddan/ddanddan/presentation/kangmin/NewMainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.ddanddan.ddanddan.service.DataLayerListenerService
 import com.ddanddan.ddanddan.util.WatchToPhoneDataSender
@@ -20,6 +21,8 @@ class NewMainActivity: ComponentActivity() {
     lateinit var watchToPhoneDataSender: WatchToPhoneDataSender
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         setContent {
             DDanDDanTheme {
