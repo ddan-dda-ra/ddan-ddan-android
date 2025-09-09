512cc91fa9782c259ff72ca256a228b3a09ea1d5
blueme0 <ksytpdus0304@gmail.com>
2025-09-09 14:52:03 +0900

    [feat/#145] 5레벨 이상일 때 펫 그래픽 5레벨

--- app/src/main/java/com/ddanddan/ddanddan/util/PetTypeExtension.kt
+++ app/src/main/java/com/ddanddan/ddanddan/util/PetTypeExtension.kt
@@ -8,51 +8,51 @@ fun PetTypeEnum?.toAnimal(level: Int?): Int {
     return when (this) {
         PetTypeEnum.CAT -> {
             when (level) {
+                1 -> R.drawable.ic_cat_level1
                 2 -> R.drawable.ic_cat_level2
                 3 -> R.drawable.ic_cat_level3
                 4 -> R.drawable.ic_cat_level4
-                5 -> R.drawable.ic_cat_level5
-                else -> R.drawable.ic_cat_level1
+                else -> R.drawable.ic_cat_level5
             }
         }
 
         PetTypeEnum.DOG -> {
             when (level) {
+                1 -> R.drawable.ic_dog_level1
                 2 -> R.drawable.ic_dog_level2
                 3 -> R.drawable.ic_dog_level3
                 4 -> R.drawable.ic_dog_level4
-                5 -> R.drawable.ic_dog_level5
-                else -> R.drawable.ic_dog_level1
+                else -> R.drawable.ic_dog_level5
             }
         }
 
         PetTypeEnum.PENGUIN -> {
             when (level) {
+                1 -> R.drawable.ic_penguin_level1
                 2 -> R.drawable.ic_penguin_level2
                 3 -> R.drawable.ic_penguin_level3
                 4 -> R.drawable.ic_penguin_level4
-                5 -> R.drawable.ic_penguin_level5
-                else -> R.drawable.ic_penguin_level1
+                else -> R.drawable.ic_penguin_level5
             }
         }
 
         PetTypeEnum.HAMSTER -> {
             when (level) {
+                1 -> R.drawable.ic_hamster_level1
                 2 -> R.drawable.ic_hamster_level2
                 3 -> R.drawable.ic_hamster_level3
                 4 -> R.drawable.ic_hamster_level4
-                5 -> R.drawable.ic_hamster_level5
-                else -> R.drawable.ic_hamster_level1
+                else -> R.drawable.ic_hamster_level5
             }
         }
 
         PetTypeEnum.MOLE -> {
             when (level) {
+                1 -> R.drawable.ic_mole_level1
                 2 -> R.drawable.ic_mole_level2
                 3 -> R.drawable.ic_mole_level3
                 4 -> R.drawable.ic_mole_level4
-                5 -> R.drawable.ic_mole_level5
-                else -> R.drawable.ic_mole_level1
+                else -> R.drawable.ic_mole_level5
             }
         }
 
@@ -65,19 +65,19 @@ fun PetTypeEnum?.toLottie(level: Int?, isPlayAndEatLottie: Boolean): Int {
         PetTypeEnum.CAT -> {
             when (isPlayAndEatLottie) {
                 false -> when (level) {
+                    1 -> R.raw.motion_cat_level1_default
                     2 -> R.raw.motion_cat_level2_default
                     3 -> R.raw.motion_cat_level3_default
                     4 -> R.raw.motion_cat_level4_default
-                    5 -> R.raw.motion_cat_level5_default
-                    else -> R.raw.motion_cat_level1_default
+                    else -> R.raw.motion_cat_level5_default
                 }
 
                 else -> when (level) {
+                    1 -> R.raw.motion_cat_level1_play_eat
                     2 -> R.raw.motion_cat_level2_play_eat
                     3 -> R.raw.motion_cat_level3_play_eat
                     4 -> R.raw.motion_cat_level4_play_eat
-                    5 -> R.raw.motion_cat_level5_play_eat
-                    else -> R.raw.motion_cat_level1_play_eat
+                    else -> R.raw.motion_cat_level5_play_eat
                 }
             }
         }
@@ -85,19 +85,19 @@ fun PetTypeEnum?.toLottie(level: Int?, isPlayAndEatLottie: Boolean): Int {
         PetTypeEnum.DOG -> {
             when (isPlayAndEatLottie) {
                 false -> when (level) {
+                    1 -> R.raw.motion_puppy_level1_default
                     2 -> R.raw.motion_puppy_level2_default
                     3 -> R.raw.motion_puppy_level3_default
                     4 -> R.raw.motion_puppy_level4_default
-                    5 -> R.raw.motion_puppy_level5_default
-                    else -> R.raw.motion_puppy_level1_default
+                    else -> R.raw.motion_puppy_level5_default
                 }
 
                 else -> when (level) {
+                    1 -> R.raw.motion_puppy_level1_play_eat
                     2 -> R.raw.motion_puppy_level2_play_eat
                     3 -> R.raw.motion_puppy_level3_play_eat
                     4 -> R.raw.motion_puppy_level4_play_eat
-                    5 -> R.raw.motion_puppy_level5_play_eat
-                    else -> R.raw.motion_puppy_level1_play_eat
+                    else -> R.raw.motion_puppy_level5_play_eat
                 }
             }
         }
@@ -105,19 +105,19 @@ fun PetTypeEnum?.toLottie(level: Int?, isPlayAndEatLottie: Boolean): Int {
         PetTypeEnum.PENGUIN -> {
             when (isPlayAndEatLottie) {
                 false -> when (level) {
+                    1 -> R.raw.motion_penguin_level1_default
                     2 -> R.raw.motion_penguin_level2_default
                     3 -> R.raw.motion_penguin_level3_default
                     4 -> R.raw.motion_penguin_level4_default
-                    5 -> R.raw.motion_penguin_level5_default
-                    else -> R.raw.motion_penguin_level1_default
+                    else -> R.raw.motion_penguin_level5_default
                 }
 
                 else -> when (level) {
+                    1 -> R.raw.motion_penguin_level1_play_eat
                     2 -> R.raw.motion_penguin_level2_play_eat
                     3 -> R.raw.motion_penguin_level3_play_eat
                     4 -> R.raw.motion_penguin_level4_play_eat
-                    5 -> R.raw.motion_penguin_level5_play_eat
-                    else -> R.raw.motion_penguin_level1_play_eat
+                    else -> R.raw.motion_penguin_level5_play_eat
                 }
             }
         }
@@ -125,19 +125,19 @@ fun PetTypeEnum?.toLottie(level: Int?, isPlayAndEatLottie: Boolean): Int {
         PetTypeEnum.HAMSTER -> {
             when (isPlayAndEatLottie) {
                 false -> when (level) {
+                    1 -> R.raw.motion_hamster_level1_default
                     2 -> R.raw.motion_hamster_level2_default
                     3 -> R.raw.motion_hamster_level3_default
                     4 -> R.raw.motion_hamster_level4_default
-                    5 -> R.raw.motion_hamster_level5_default
-                    else -> R.raw.motion_hamster_level1_default
+                    else -> R.raw.motion_hamster_level5_default
                 }
 
                 else -> when (level) {
+                    1 -> R.raw.motion_hamster_level1_play_eat
                     2 -> R.raw.motion_hamster_level2_play_eat
                     3 -> R.raw.motion_hamster_level3_play_eat
                     4 -> R.raw.motion_hamster_level4_play_eat
-                    5 -> R.raw.motion_hamster_level5_play_eat
-                    else -> R.raw.motion_hamster_level1_play_eat
+                    else -> R.raw.motion_hamster_level5_play_eat
                 }
             }
         }
@@ -145,19 +145,19 @@ fun PetTypeEnum?.toLottie(level: Int?, isPlayAndEatLottie: Boolean): Int {
         PetTypeEnum.MOLE -> {
             when (isPlayAndEatLottie) {
                 false -> when (level) {
+                    1 -> R.raw.motion_mole_level1_default
                     2 -> R.raw.motion_mole_level2_default
                     3 -> R.raw.motion_mole_level3_default
                     4 -> R.raw.motion_mole_level4_default
-                    5 -> R.raw.motion_mole_level5_dafault
-                    else -> R.raw.motion_mole_level1_default
+                    else -> R.raw.motion_mole_level5_default
                 }
 
                 else -> when (level) {
+                    1 -> R.raw.motion_mole_level1_play_eat
                     2 -> R.raw.motion_mole_level2_play_eat
                     3 -> R.raw.motion_mole_level3_play_eat
                     4 -> R.raw.motion_mole_level4_play_eat
-                    5 -> R.raw.motion_mole_level5_play_eat
-                    else -> R.raw.motion_mole_level1_play_eat
+                    else -> R.raw.motion_mole_level5_play_eat
                 }
             }
         }
