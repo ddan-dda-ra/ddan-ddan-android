88f484a13a0d0f12bf5bbe77aaf5f600206cc9ba
Leekangmin <kkk5474096@naver.com>
2025-09-09 10:09:39 +0900

    [feature/#146] version up 23

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ddanddan.ddanddan"
         minSdk = 30
         targetSdk = 35
-        versionCode = 22
+        versionCode = 23
         versionName = "1.0"
     }
 
