bfb9e34f49ae488a3023add614932ec002eac373
blueme0 <ksytpdus0304@gmail.com>
2025-09-09 15:10:32 +0900

    [feat/#145] 홈 상단 아이콘 제거

--- app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeContract.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/home/HomeContract.kt
@@ -19,12 +19,10 @@ data class HomeState(
 )
 
 sealed class HomeSideEffect {
-    object NavigateSetting: HomeSideEffect()
     data class NetworkError(val code: Int?) : HomeSideEffect()
     data class NavigateLevelUp(val level: Int, val petType: PetTypeEnum) : HomeSideEffect()
     data class NavigateNewPet(val petType: PetTypeEnum) : HomeSideEffect()
 //    data class NavigatePetCollection(val petId: String) : HomeSideEffect()
-    object NavigateRanking: HomeSideEffect()
     object AskNotification: HomeSideEffect()
     data class SnackBarMsg(val msg: String) : HomeSideEffect()
 }
