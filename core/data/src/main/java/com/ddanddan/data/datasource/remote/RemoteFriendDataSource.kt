bf3a6c9519e548788357484a7480c341356f8ca3
blueme0 <ksytpdus0304@gmail.com>
2025-10-06 14:42:24 +0900

    [feat/#152] RemoteFriendDataSource

---
+++ core/data/src/main/java/com/ddanddan/data/datasource/remote/RemoteFriendDataSource.kt
@@ -0,0 +1,15 @@
+package com.ddanddan.data.datasource.remote
+
+import com.ddanddan.data.service.FriendService
+import com.ddanddan.model.response.ResponseFriend
+import javax.inject.Inject
+
+class RemoteFriendDataSource @Inject constructor(
+    private val friendService: FriendService
+) {
+    suspend fun getFriendsList(): List<ResponseFriend> = friendService.getFriendsList().friends
+    suspend fun getInviteCode(): String = friendService.postInviteCode().code
+    suspend fun postInviteFriend(code: String): ResponseFriend = friendService.postInviteFriend(code).friendUser
+    suspend fun deleteFriend(fId: String) = friendService.deleteFriend(fId)
+    suspend fun postCheers(fId: String) = friendService.postCheers(fId)
+}
