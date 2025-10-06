56d9465eb70587cf3a9fff6cd26d9deae7426d94
blueme0 <ksytpdus0304@gmail.com>
2025-10-06 14:43:38 +0900

    [feat/#152] FriendsContract

--- app/src/main/java/com/ddanddan/ddanddan/presentation/friends/FriendsContract.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/friends/FriendsContract.kt
@@ -6,9 +6,14 @@ import com.ddanddan.domain.entity.Friend
 @Immutable
 data class FriendsState(
     val myProfile: Friend? = null,
-    val friends: List<Friend> = listOf()
+    val friends: List<Friend> = listOf(),
+    val myInviteLink: String? = null,
+    val isShowDeleteDialog: Boolean = false,
+    val chosenFriendId: String? = null
 )
 
 sealed class FriendsSideEffect {
+    object RefreshList : FriendsSideEffect()
+    data class CopyInviteLink(val link: String) : FriendsSideEffect()
     data class NetworkError(val msg: String): FriendsSideEffect()
 }
