f7e6b42481e410b080ceedc0ae295567cb3526d9
Leekangmin <kkk5474096@naver.com>
2025-10-13 18:15:25 +0900

    [feature/#159] PetTypeEnum 확장함수 구현

--- core/domain/src/main/java/com/ddanddan/domain/enums/PetTypeEnum.kt
+++ core/domain/src/main/java/com/ddanddan/domain/enums/PetTypeEnum.kt
@@ -3,3 +3,20 @@ package com.ddanddan.domain.enums
 enum class PetTypeEnum {
     CAT, HAMSTER, PENGUIN, DOG, MOLE
 }
+
+fun PetTypeEnum.toString() = when(this) {
+    PetTypeEnum.CAT -> "CAT"
+    PetTypeEnum.HAMSTER -> "HAMSTER"
+    PetTypeEnum.PENGUIN -> "PENGUIN"
+    PetTypeEnum.DOG -> "DOG"
+    PetTypeEnum.MOLE -> "MOLE"
+}
+
+fun String.toPetTypeEnum() = when(this) {
+    "CAT" -> PetTypeEnum.CAT
+    "HAMSTER" -> PetTypeEnum.HAMSTER
+    "PENGUIN" -> PetTypeEnum.PENGUIN
+    "DOG" -> PetTypeEnum.DOG
+    "MOLE" -> PetTypeEnum.MOLE
+    else -> PetTypeEnum.CAT
+}
