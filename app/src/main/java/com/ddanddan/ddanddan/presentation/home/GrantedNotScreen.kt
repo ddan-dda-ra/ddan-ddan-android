7bd5fa1310f81a8bccd4ed42b5f651784706205a
blueme0 <ksytpdus0304@gmail.com>
2026-04-15 01:25:37 +0900

    [feat/#177] GrantedNotScreen UI 및 화면 분기 수정

--- app/src/main/java/com/ddanddan/ddanddan/presentation/home/GrantedNotScreen.kt
+++ app/src/main/java/com/ddanddan/ddanddan/presentation/home/GrantedNotScreen.kt
@@ -5,28 +5,39 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -57,7 +68,7 @@ fun GrantedNotRoute(
     }
     GrantedNotScreen(
         onPermissionGranted = {
-            viewModel.isFirstAfterInstall()
+            viewModel.isAutoLoginEnabled()
         }
     )
 }
@@ -68,6 +79,8 @@ fun GrantedNotScreen(
     val context = LocalContext.current
     val activity = context as? ComponentActivity
     val lifecycleOwner = LocalLifecycleOwner.current
+
+    var isShowWarning by remember { mutableStateOf(false) }
 
     // 권한 요청 런처
     val permissionLauncher = rememberLauncherForActivityResult(
@@ -128,54 +141,93 @@ fun GrantedNotScreen(
         }
     }
 
-    Column(
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(DDanDDanColorPalette.current.color_background)
+            .padding(horizontal = 24.dp)
     ) {
-        Text(
-            text = "권한이 필요합니다",
-            style = DDanDDanTypo.current.HeadLine5,
-            textAlign = TextAlign.Center,
-            color = DDanDDanColorPalette.current.color_text_headline_primary
-        )
+        val (card, warning) = createRefs()
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "이 앱은 건강 데이터를 수집하여 칼로리 소모량을 계산하기 위해 생체 신호 센서 권한이 필요합니다. 권한을 허용하지 않으면 앱의 핵심 기능을 사용하실 수 없습니다.",
-            style = DDanDDanTypo.current.HeadLine5,
-            textAlign = TextAlign.Center,
-            color = DDanDDanColorPalette.current.color_text_headline_primary
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Button(
-            onClick = onButtonClick,
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = DDanDDanColorPalette.current.color_button_active,
-                contentColor = DDanDDanColorPalette.current.color_text_button_primary_default
-            ),
-            elevation = ButtonDefaults.elevation(0.dp),
-            shape = RoundedCornerShape(16.dp)
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .constrainAs(card) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .clip(RoundedCornerShape(8.dp))
+                .background(DDanDDanColorPalette.current.elevation_color_elevation_level01)
+                .padding(top = 40.dp, start = 20.dp, end = 20.dp, bottom = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            androidx.compose.material.Text(
-                text = "권한 허용하기",
-                style = DDanDDanTypo.current.HeadLine6
+            Text(
+                text = "생체 신호 센서 접근 권한을 허용해주세요",
+                style = DDanDDanTypo.current.HeadLine6,
+                color = DDanDDanColorPalette.current.color_text_headline_primary
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "서비스 이용을 위하여\n생체 신호 센서 접근 권한이 필요합니다.",
+                style = DDanDDanTypo.current.Body2,
+                color = DDanDDanColorPalette.current.color_text_body_teritary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(28.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(
+                    onClick = { isShowWarning = true },
+                    modifier = Modifier
+                        .weight(1f),
+                    contentPadding = PaddingValues(vertical = 17.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = DDanDDanColorPalette.current.color_button_alternative,
+                        contentColor = DDanDDanColorPalette.current.color_text_button_alternative
+                    ),
+                    elevation = ButtonDefaults.elevation(0.dp),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(text = "허용 안함", style = DDanDDanTypo.current.HeadLine6, color = DDanDDanColorPalette.current.color_text_button_alternative)
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Button(
+                    onClick = { onButtonClick() },
+                    modifier = Modifier
+                        .weight(1f),
+                    contentPadding = PaddingValues(vertical = 17.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = DDanDDanColorPalette.current.color_button_active,
+                        contentColor = DDanDDanColorPalette.current.color_text_button_primary_default
+                    ),
+                    elevation = ButtonDefaults.elevation(0.dp),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(text = "허용", style = DDanDDanTypo.current.HeadLine6, color = DDanDDanColorPalette.current.color_text_button_primary_default)
+                }
+            }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "권한을 허용하지 않으면 앱을 사용하실 수 없습니다.",
-            style = DDanDDanTypo.current.Body2,
-            color = DDanDDanColorPalette.current.color_text_body_secondary,
-            textAlign = TextAlign.Center
-        )
+        if (isShowWarning) {
+            Text(
+                modifier = Modifier.constrainAs(warning) {
+                    top.linkTo(card.bottom, margin = 24.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                text = "이 앱은 건강 데이터를 수집하여 칼로리 소모량을 계산하기 위해 생체 신호 센서 권한이 필요합니다. 권한을 허용하지 않으면 앱의 핵심 기능을 사용하실 수 없습니다.",
+                style = DDanDDanTypo.current.Body2,
+                color = DDanDDanColorPalette.current.color_text_body_teritary,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
